bffb59a3d788674c6c20e5652a49d531056a9725
NightEpiphany <nightepiphany0401@gmail.com>
2026-02-08 22:42:19 +0800

    修复手臂动画

--- src/main/java/com/github/ysbbbbbb/kaleidoscopecookery/mixin/realize/HumanoidModelMixin.java
+++ src/main/java/com/github/ysbbbbbb/kaleidoscopecookery/mixin/realize/HumanoidModelMixin.java
@@ -15,7 +15,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Deprecated
 @Environment(EnvType.CLIENT)
 @Mixin(HumanoidModel.class)
 public abstract class HumanoidModelMixin<T extends HumanoidRenderState> extends EntityModel<T> {
