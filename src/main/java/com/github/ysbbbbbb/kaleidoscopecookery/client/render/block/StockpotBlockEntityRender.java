49a5afb205b7cb5576e2a4a0b1b05a4319cb7a0b
NightEpiphany <nightepiphany0401@gmail.com>
2026-03-30 14:48:34 +0800

    修复部分问题

--- src/main/java/com/github/ysbbbbbb/kaleidoscopecookery/client/render/block/StockpotBlockEntityRender.java
+++ src/main/java/com/github/ysbbbbbb/kaleidoscopecookery/client/render/block/StockpotBlockEntityRender.java
@@ -69,14 +69,14 @@ public class StockpotBlockEntityRender implements BlockEntityRenderer<StockpotBl
         blockEntityRenderState.items = new ArrayList<>();
         blockEntityRenderState.randomSeeds = new ArrayList<>();
         blockEntityRenderState.status = blockEntity.getStatus();
-        blockEntityRenderState.hasLiquidIngredients = blockEntity.hasContainerIngredients();
+        blockEntityRenderState.hasLiquidIngredients = blockEntity.liquidMerged();
         for (var index = 0; index < blockEntity.getInputs().size(); index++) {
             ItemStackRenderState itemStackRenderState = new ItemStackRenderState();
             ItemStack itemStack = blockEntity.getInputs().get(index);
             if (itemStack.is(TagMod.SPECIAL)) {
                 itemStack.set(ModDataComponents.SPECIAL_RENDER, true);
             }
-            if (!ItemUtils.getContainerItem(itemStack).getDefaultInstance().isEmpty() && !itemStack.is(TagMod.SPECIAL))
+            if (!(ItemUtils.getContainerItem(itemStack).getDefaultInstance().isEmpty() || itemStack.is(TagMod.SPECIAL)))
                 continue;
             this.itemModelResolver.updateForTopItem(itemStackRenderState, itemStack, ItemDisplayContext.FIXED, blockEntity.getLevel(), null, posLong + index);
             blockEntityRenderState.items.add(itemStackRenderState);
