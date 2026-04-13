f241dbd6b61dbb41129e490121cebe7b32afa7dd
NightEpiphany <nightepiphany0401@gmail.com>
2026-04-14 00:04:30 +0800

    修复部分问题

--- src/main/java/com/github/ysbbbbbb/kaleidoscopecookery/init/ModVillager.java
+++ src/main/java/com/github/ysbbbbbb/kaleidoscopecookery/init/ModVillager.java
@@ -10,9 +10,14 @@ import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.entity.npc.villager.VillagerProfession;
+import net.minecraft.world.level.storage.loot.LootTable;
 
 @SuppressWarnings("all")
 public class ModVillager {
+    public static final Identifier CHEF_GIFT = Identifier.fromNamespaceAndPath(KaleidoscopeCookery.MOD_ID, "gameplay/hero_of_the_village/chef_gift");
+
+    public static final ResourceKey<LootTable> CHEF_GIFT_LOOT_KEY = ResourceKey.create(Registries.LOOT_TABLE, CHEF_GIFT);
+
     public static final VillagerProfession CHEF_VALUE = new VillagerProfession(Component.translatable("entity.minecraft.villager.chef"),
             poi -> poi.value() == ModPoi.STOVE,
             poi -> poi.value() == ModPoi.STOVE,
