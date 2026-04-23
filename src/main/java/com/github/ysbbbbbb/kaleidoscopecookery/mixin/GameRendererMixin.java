73d1b526cae101c29d61a17241b1d0a4b5ea68f7
NightEpiphany <nightepiphany0401@gmail.com>
2026-04-23 20:04:37 +0800

    修复mixin启动崩溃

--- src/main/java/com/github/ysbbbbbb/kaleidoscopecookery/mixin/GameRendererMixin.java
+++ src/main/java/com/github/ysbbbbbb/kaleidoscopecookery/mixin/GameRendererMixin.java
@@ -8,7 +8,9 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.Camera;
 import net.minecraft.client.renderer.GameRenderer;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -16,6 +18,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Environment(EnvType.CLIENT)
 @Mixin(GameRenderer.class)
 public class GameRendererMixin {
+
+    @Shadow
+    @Final
+    private Camera mainCamera;
 
     @Inject(
             method = "renderLevel",
@@ -25,12 +31,12 @@ public class GameRendererMixin {
                     shift = At.Shift.AFTER
             )
     )
-    private void renderLevel(float f, long l, PoseStack poseStack, CallbackInfo ci, @Local(name = "camera")Camera camera) {
-        var event = new ViewportEvent.ComputeCameraAngles(((GameRenderer) (Object) this), camera, f, camera.getYRot(), camera.getXRot(), 0);
+    private void renderLevel(float f, long l, PoseStack poseStack, CallbackInfo ci) {
+        var event = new ViewportEvent.ComputeCameraAngles(((GameRenderer) (Object) this), this.mainCamera, f, this.mainCamera.getYRot(), this.mainCamera.getXRot(), 0);
         event.post();
         // setup() 会覆盖相机旋转，所以要在其执行后再应用事件修改。
-        camera.yRot = event.getYaw();
-        camera.xRot = event.getPitch();
+        this.mainCamera.yRot = event.getYaw();
+        this.mainCamera.xRot = event.getPitch();
         poseStack.mulPose(Axis.ZP.rotationDegrees(event.getRoll()));
     }
 }
