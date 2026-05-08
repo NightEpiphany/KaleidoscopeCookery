f075bb1909d33ebbe0b171883b5be04847e588af
NightEpiphany <nightepiphany0401@gmail.com>
2026-05-08 15:55:31 +0800

    完善RRV显示

--- src/main/java/com/github/ysbbbbbb/kaleidoscopecookery/compat/rrv/teapot/TeapotViewRecipe.java
+++ src/main/java/com/github/ysbbbbbb/kaleidoscopecookery/compat/rrv/teapot/TeapotViewRecipe.java
@@ -4,6 +4,7 @@ import cc.cassian.rrv.api.recipe.ReliableClientRecipe;
 import cc.cassian.rrv.api.recipe.ReliableClientRecipeType;
 import cc.cassian.rrv.common.recipe.inventory.RecipeViewMenu;
 import cc.cassian.rrv.common.recipe.inventory.SlotContent;
+import com.github.ysbbbbbb.kaleidoscopecookery.crafting.recipe.TeapotRecipe;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.Ingredient;
 
@@ -13,11 +14,13 @@ public class TeapotViewRecipe implements ReliableClientRecipe {
     private final Ingredient ingredient;
     private final ItemStack result;
     private final ItemStack teaFluid;
+    private final int ingredientCount;
 
     public TeapotViewRecipe(TeapotServerRecipe recipe) {
         this.ingredient = recipe.getIngredient();
         this.result = recipe.getResult();
         this.teaFluid = recipe.getTeaFluid();
+        this.ingredientCount = recipe.getIngredientCount();
     }
 
     @Override
@@ -27,9 +30,10 @@ public class TeapotViewRecipe implements ReliableClientRecipe {
 
     @Override
     public void bindSlots(RecipeViewMenu.SlotFillContext slotFillContext) {
+        var checkedIngredient = this.ingredient.items().map(s -> s.value().getDefaultInstance().copyWithCount(this.ingredientCount)).toList();
         slotFillContext.bindSlot(0, SlotContent.of(this.teaFluid));
-        slotFillContext.bindSlot(1, SlotContent.of(this.ingredient));
-        slotFillContext.bindSlot(2, SlotContent.of(this.result));
+        slotFillContext.bindSlot(1, SlotContent.of(checkedIngredient));
+        slotFillContext.bindSlot(2, SlotContent.of(this.result.copyWithCount(TeapotRecipe.OUTPUT_COUNT)));
     }
 
     @Override
