fe3d1d2bcd1b4913086bd35fb02d0f5b038aaac5
NightEpiphany <nightepiphany0401@gmail.com>
2026-07-29 21:01:52 +0800

    Loot Api回来了

--- src/main/java/com/github/ysbbbbbb/kaleidoscopecookery/compat/trinkets/init/ModTrinketsCompat.java
+++ src/main/java/com/github/ysbbbbbb/kaleidoscopecookery/compat/trinkets/init/ModTrinketsCompat.java
@@ -6,6 +6,7 @@ import com.github.ysbbbbbb.kaleidoscopecookery.compat.trinkets.aglets.StrawHatTr
 import com.github.ysbbbbbb.kaleidoscopecookery.init.ModItems;
 import com.github.ysbbbbbb.kaleidoscopecookery.init.tag.TagMod;
 import com.github.ysbbbbbb.kaleidoscopecookery.util.PortHelper;
+import net.fabricmc.fabric.api.loot.v3.LootTableEvents;
 import net.minecraft.advancements.predicates.ItemPredicate;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.Registry;
@@ -31,35 +32,35 @@ public class ModTrinketsCompat {
 
     @Contract(pure = true)
     static void init() {
-//            STRAW_HAT = registerStrawHatsTrinket("straw_hat", new StrawHatTrinketItem(false, new Item.Properties().setId(PortHelper.createItemId("straw_hat"))));
-//            STRAW_HAT_FLOWER = registerStrawHatsTrinket("straw_hat_flower", new StrawHatTrinketItem(true, new Item.Properties().setId(PortHelper.createItemId("straw_hat_flower"))));
-//                Registry.register(BuiltInRegistries.LOOT_CONDITION_TYPE, StrawHatTrinketsLootCondition.ID, StrawHatTrinketsLootCondition.CODEC);
-//                LootTableEvents.MODIFY.register((key, tableBuilder, _, wrapperLookup) -> {
-//                    Identifier id = key.identifier();
-//                    HolderLookup.RegistryLookup<Enchantment> enchantment = wrapperLookup.lookupOrThrow(Registries.ENCHANTMENT);
-//                    if (id.equals(Identifier.withDefaultNamespace("blocks/short_grass"))) {
-//                        HolderLookup<Item> holderLookup = wrapperLookup.lookupOrThrow(Registries.ITEM);
-//                        var predicate = ItemPredicate.Builder.item().of(holderLookup, TagMod.STRAW_HAT).build();
-//                        var enchantments = ApplyBonusCount.addUniformBonusCount(enchantment.getOrThrow(Enchantments.FORTUNE), 2);
-//                        var chance = LootItemRandomChanceCondition.randomChance(0.125F);
-//                        var match = StrawHatTrinketsLootCondition.toolMatches(predicate);
-//                        tableBuilder.withPool(LootPool.lootPool()
-//                                .setRolls(ConstantValue.exactly(1))
-//                                .add(LootItem.lootTableItem(ModItems.TOMATO_SEED)
-//                                        .when(chance).when(match)
-//                                        .apply(enchantments))
-//                                .add(LootItem.lootTableItem(ModItems.CHILI_SEED)
-//                                        .when(chance).when(match)
-//                                        .apply(enchantments))
-//                                .add(LootItem.lootTableItem(ModItems.LETTUCE_SEED)
-//                                        .when(chance).when(match)
-//                                        .apply(enchantments))
-//                                .add(LootItem.lootTableItem(ModItems.WILD_RICE_SEED)
-//                                        .when(chance).when(match)
-//                                        .apply(enchantments))
-//                                .add(EmptyLootItem.emptyItem().setWeight(2)));
-//                    }
-//                });
+            STRAW_HAT = registerStrawHatsTrinket("straw_hat", new StrawHatTrinketItem(false, new Item.Properties().setId(PortHelper.createItemId("straw_hat"))));
+            STRAW_HAT_FLOWER = registerStrawHatsTrinket("straw_hat_flower", new StrawHatTrinketItem(true, new Item.Properties().setId(PortHelper.createItemId("straw_hat_flower"))));
+                Registry.register(BuiltInRegistries.LOOT_CONDITION_TYPE, StrawHatTrinketsLootCondition.ID, StrawHatTrinketsLootCondition.CODEC);
+                LootTableEvents.MODIFY.register((key, tableBuilder, _, wrapperLookup) -> {
+                    Identifier id = key.identifier();
+                    HolderLookup.RegistryLookup<Enchantment> enchantment = wrapperLookup.lookupOrThrow(Registries.ENCHANTMENT);
+                    if (id.equals(Identifier.withDefaultNamespace("blocks/short_grass"))) {
+                        HolderLookup<Item> holderLookup = wrapperLookup.lookupOrThrow(Registries.ITEM);
+                        var predicate = ItemPredicate.Builder.item().of(holderLookup, TagMod.STRAW_HAT).build();
+                        var enchantments = ApplyBonusCount.addUniformBonusCount(enchantment.getOrThrow(Enchantments.FORTUNE), 2);
+                        var chance = LootItemRandomChanceCondition.randomChance(0.125F);
+                        var match = StrawHatTrinketsLootCondition.toolMatches(predicate);
+                        tableBuilder.withPool(LootPool.lootPool()
+                                .setRolls(ConstantValue.exactly(1))
+                                .add(LootItem.lootTableItem(ModItems.TOMATO_SEED)
+                                        .when(chance).when(match)
+                                        .apply(enchantments))
+                                .add(LootItem.lootTableItem(ModItems.CHILI_SEED)
+                                        .when(chance).when(match)
+                                        .apply(enchantments))
+                                .add(LootItem.lootTableItem(ModItems.LETTUCE_SEED)
+                                        .when(chance).when(match)
+                                        .apply(enchantments))
+                                .add(LootItem.lootTableItem(ModItems.WILD_RICE_SEED)
+                                        .when(chance).when(match)
+                                        .apply(enchantments))
+                                .add(EmptyLootItem.emptyItem().setWeight(2)));
+                    }
+                });
     }
 
     public static Supplier<Item> registerStrawHatsTrinket(String string, Item item) {
