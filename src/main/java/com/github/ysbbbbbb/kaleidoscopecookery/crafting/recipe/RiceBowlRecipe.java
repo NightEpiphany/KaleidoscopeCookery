1ce639589c03be71f11da5b0f7efd48ad92167a5
NightEpiphany <nightepiphany0401@gmail.com>
2026-08-19 21:39:57 +0800

    盖浇饭配方修复

--- src/main/java/com/github/ysbbbbbb/kaleidoscopecookery/crafting/recipe/RiceBowlRecipe.java
+++ src/main/java/com/github/ysbbbbbb/kaleidoscopecookery/crafting/recipe/RiceBowlRecipe.java
@@ -52,6 +52,18 @@ public class RiceBowlRecipe extends CustomRecipe {
         return assembled;
     }
 
+    @Override
+    public @NotNull NonNullList<ItemStack> getRemainingItems(@NonNull CraftingInput container) {
+        NonNullList<ItemStack> remainingItems = CraftingRecipe.defaultCraftingReminder(container);
+        for (int i = 0; i < container.size(); i++) {
+            if (COOKED_RICE.test(container.getItem(i))) {
+                // 米饭的碗成为盖浇饭容器，不应作为合成剩余物返还。
+                remainingItems.set(i, ItemStack.EMPTY);
+            }
+        }
+        return remainingItems;
+    }
+
 
     public boolean canCraftInDimensions(int width, int height) {
         return width * height >= 2;
@@ -69,7 +81,10 @@ public class RiceBowlRecipe extends CustomRecipe {
 
 
     public @NotNull NonNullList<Ingredient> getIngredients() {
-        return NonNullList.of(this.ingredient, COOKED_RICE);
+        NonNullList<Ingredient> ingredients = NonNullList.create();
+        ingredients.add(this.ingredient);
+        ingredients.add(COOKED_RICE);
+        return ingredients;
     }
 
 
