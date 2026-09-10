b34a50d598e30048a90b99d3e4b79e76cede55d6
NightEpiphany <nightepiphany0401@gmail.com>
2026-09-11 00:17:48 +0800

    修复配方平放朝向

--- src/main/java/com/github/ysbbbbbb/kaleidoscopecookery/block/decoration/RecipeBlock.java
+++ src/main/java/com/github/ysbbbbbb/kaleidoscopecookery/block/decoration/RecipeBlock.java
@@ -125,6 +125,9 @@ public class RecipeBlock extends FaceAttachedHorizontalDirectionalBlock implemen
         FluidState fluidState = context.getLevel().getFluidState(context.getClickedPos());
         BlockState stateForPlacement = super.getStateForPlacement(context);
         if (stateForPlacement != null) {
+            if (stateForPlacement.getValue(FACE) == AttachFace.FLOOR || stateForPlacement.getValue(FACE) == AttachFace.CEILING)
+                stateForPlacement = stateForPlacement.setValue(FACING,
+                        context.getHorizontalDirection().getOpposite());
             return stateForPlacement.setValue(WATERLOGGED, fluidState.getType() == Fluids.WATER);
         }
         return null;
