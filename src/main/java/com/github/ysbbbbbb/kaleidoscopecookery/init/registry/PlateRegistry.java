8024710726fdc1f2f0674b647e7f0b9d897cb576
NightEpiphany <126158635+NightEpiphany@users.noreply.github.com>
2026-09-12 00:07:31 +0800

    规范化

--- src/main/java/com/github/ysbbbbbb/kaleidoscopecookery/init/registry/PlateRegistry.java
+++ src/main/java/com/github/ysbbbbbb/kaleidoscopecookery/init/registry/PlateRegistry.java
@@ -19,7 +19,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.function.Supplier;
 
-public class PlateRegistry {
+public final class PlateRegistry {
     public static final Map<ResourceLocation, PlateData> PLATE_DATA_MAP = Maps.newLinkedHashMap();
 
     public static ResourceLocation SHENGJIAN_MANTOU_PLATE;
